524c94b810534c87d45eee322abc1974bc23fb61
Raja <shraja.1806@gmail.com>
2022-11-17 18:47:01 +0530

    completed

--- src/Components/News.js
+++ src/Components/News.js
@@ -4,7 +4,13 @@ import Spinner from "./Spinner";
 import PropTypes from "prop-types";
 import InfiniteScroll from "react-infinite-scroll-component";
 
-const News = (props) => {
+const News = ({
+   country,
+   category,
+   apiKey,
+   pageSize,
+   setProgress 
+}) => {
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(true);
   const [page, setPage] = useState(1);
@@ -16,30 +22,30 @@ const News = (props) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
   const updateNews = async () => {
-    props.setProgress(10);
-    let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+    setProgress(10);
+    let url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}&page=${page}&pageSize=${pageSize}`;
     setLoading(true);
     let data = await fetch(url);
-    props.setProgress(30);
+    setProgress(30);
     let parsedData = await data.json();
-    props.setProgress(70);
+    setProgress(70);
     setArticles(parsedData.articles);
     setTotalResults(parsedData.totalResults);
     setLoading(false);
-    props.setProgress(100);
+    setProgress(100);
   };
   useEffect(() => {
-    document.title = `${capitalizeFirstLetter(props.category)} - Daily News`;
+    document.title = `${capitalizeFirstLetter(category)} - Daily News`;
     updateNews();
-    // eslint-disabled-next-line
-  }, []);
+   
+  }, [category]);
 
   const fetchMoreData = async () => {
     let url = `https://newsapi.org/v2/top-headlines?country=${
-      props.country
-    }&category=${props.category}&apiKey=${props.apiKey}&page=${
+      country
+    }&category=${category}&apiKey=${apiKey}&page=${
       page + 1
-    }&pageSize=${props.pageSize}`;
+    }&pageSize=${pageSize}`;
     setPage(page + 1);
     let data = await fetch(url);
     let parsedData = await data.json();
@@ -50,7 +56,7 @@ const News = (props) => {
   return (
     <>
       <h1 className="text-center" style={{ margin: "35px, 0" , marginTop : "90px"}}>
-        Top {capitalizeFirstLetter(props.category)} Headlines
+        Top {capitalizeFirstLetter(category)} Headlines
       </h1>
       {loading && <Spinner />}
       <InfiniteScroll
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,52 +1,47 @@
 import React from "react";
 import NoImage from "./NoImage";
-
+import DefaultImage from './NoImage.png'
 
 const NewsItem = (props) => {
-    let { title, description, imageUrl, news, author, date, sourceName } = props;
+  let { title, description, imageUrl, news, author, date, sourceName } = props;
   return (
-      <div className="my-3">
-        <div className="card">
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "flex-end",
-              position: "absolute",
-              right: "0",
-            }}
+    <div className="my-3">
+      <div className="card">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            position: "absolute",
+            right: "0",
+          }}
+        >
+          <span className="badge rounded-pill bg-danger"> {sourceName}</span>
+        </div>
+        <img
+          src={imageUrl || DefaultImage}
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+          <p className="card-text">
+            <small className="text-muted">
+              By {author} on {new Date(date).toGMTString()}
+            </small>
+          </p>
+          <a
+            rel="noreferrer"
+            href={news}
+            target="_blank"
+            className="btn btn-sm btn-dark"
           >
-            <span className="badge rounded-pill bg-danger"> {sourceName}</span>
-          </div>
-          <img
-            src={
-              // imageUrl || <NoImage />
-              imageUrl?imageUrl : <NoImage />
-            }
-            className="card-img-top"
-            alt="..."
-          />
-
-          <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <p className="card-text">
-              <small className="text-muted">
-                By {author} on {new Date(date).toGMTString()}
-              </small>
-            </p>
-            <a
-              rel="noreferrer"
-              href={news}
-              target="_blank"
-              className="btn btn-sm btn-dark"
-            >
-              Read More
-            </a>
-          </div>
+            Read More
+          </a>
         </div>
       </div>
-    );
-  }
-
+    </div>
+  );
+};
 
 export default NewsItem;
